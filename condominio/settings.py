cb751bd817b67b66888ec699549a39e6f709a9ef
rafael <rafaelufjf@gmail.com>
2021-09-21 16:40:10 -0300

    criação do modelo inicial do bco de dados

--- condominio/settings.py
+++ condominio/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'condominio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql', #postgresql
+        'NAME': 'pi_apartamentos',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost'
     }
 }
 
